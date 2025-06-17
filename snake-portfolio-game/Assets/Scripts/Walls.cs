69019e373af38a2682d338901307180b6497b195
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-06-17 12:03:17 -0300

    chore: Add TextMeshPro and Finished Gameplay;

--- snake-portfolio-game/Assets/Scripts/Walls.cs
+++ snake-portfolio-game/Assets/Scripts/Walls.cs
@@ -4,6 +4,7 @@ public class Walls : MonoBehaviour
 {
     public GameObject wallBlock;
     public int setUp;
+ 
     void Start()
     {
         switch (setUp)
@@ -19,14 +20,13 @@ public class Walls : MonoBehaviour
 
     private void SetUp01()
     {
-        GameObject wb = null;
         for (int i = -7; i < 8; i++)
         {
             for (int j = -12; j < 13; j++)
             {
                 if (i == -7 || i == 7 || j == -12 || j == 12)
                 {
-                    wb = Instantiate(wallBlock);
+                    GameObject wb = Instantiate(wallBlock);
                     wb.transform.position = new Vector3(j, i, 0.0f);
                     wb.transform.parent = transform;
                 }
